c2a1d62f1c65116c7a4876eff141d71b539099e5
martine <martine@hcis>
2016-04-29 14:10:20 +0200

    .

--- ReturFire/src/com/returnfire/models/elementos/EdificioModel.java
+++ ReturFire/src/com/returnfire/models/elementos/EdificioModel.java
@@ -1,8 +1,15 @@
 package com.returnfire.models.elementos;
 
+import com.entity.modules.gui.events.IOnLeftClick;
 import com.returnfire.dao.elementos.buildings.EdificioDAO;
 
-public abstract class EdificioModel<T extends EdificioDAO> extends EstaticoModel<T> {
+public abstract class EdificioModel<T extends EdificioDAO> extends EstaticoModel<T> implements IOnLeftClick{
+
+	@Override
+	public boolean onLeftClick(boolean value, float tpf) {
+		System.out.println("On left click!!!!!"+this);
+		return true;
+	}
 
 
 
